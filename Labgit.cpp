c23a611c39a545a1f3aff013b6f2aa427901743e
unknown <avargasla@unal.edu.co>
2026-05-24 21:48:19 -0500

    El archivo se puede leer y guardar

--- Labgit.cpp
+++ Labgit.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
 using namespace std;
 int main (){
-	
+	ifstream archivoentrada ("input/texto endrada.txt");
+	if (archivoentrada.is_open()) {
+		cout<<"Archivo abierto"<<endl;
+		string linea;
+		while (getline (archivoentrada, linea)){
+			archivosalida<<linea;
+		}
+		cout<<"Archivo copiado correspondientemente"<<endl;
+	}
 	
 }
